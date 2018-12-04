59a05441118d0954265845e5a3895fbb2cae8855
vivatoviva <17051298278@163.com>
2018-12-04 21:30:25 +0800

    Iterator

---
+++ Iterator/index.js
@@ -0,0 +1,28 @@
+const Iterator = function (arr) {
+  let current = 0;
+  const next = function () {
+    current += 1;
+  }
+  const isDone = function () {
+    return current >= arr.length - 1;
+  }
+
+  const getCurrItem = function () {
+    return arr[current];
+  }
+
+  return {
+    next,
+    isDone,
+    getCurrItem,
+  }
+};
+
+// 中止迭代器 (可以自由选择方法)
+function each (arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    if (!callback(i, arr[i])) {
+      break;
+    }
+  }
+}
